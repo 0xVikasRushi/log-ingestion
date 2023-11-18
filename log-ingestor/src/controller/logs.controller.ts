cae777d6959b0926ef7a6976edf2a90bd809fd4a
0xvikasrushi <0xvikas@gmail.com>
2023-11-18 20:19:09 +0530

    🚀 RELEASE: Log Ingestor all endpoints

--- log-ingestor/src/controller/logs.controller.ts
+++ log-ingestor/src/controller/logs.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { Log } from "../types/log";
 import elasticClientInstance from "../elastic-client";
+import { Log } from "../types/log";
 
 const saveLogs = async (req: Request, res: Response) => {
   try {
@@ -49,4 +49,152 @@ const getfullSearch = async (req: Request, res: Response) => {
   }
 };
 
-export { saveLogs, getLogs, getfullSearch };
+//? http://localhost:3000/api/logs/search?param1=value1&param2=value2
+
+const querySearch = (req: Request, res: Response) => {
+  const data = req.query;
+
+  const result = elasticClientInstance.search({
+    index: "logs",
+    query: {
+      match: {
+        level: "error",
+      },
+    },
+  });
+  console.log(result + "Ssss");
+  res.send(result);
+};
+
+const getLevelSearch = async (req: Request, res: Response) => {
+  try {
+    const { levelid, size } = req.params;
+    if (!levelid) {
+      res.status(400).send({ message: "Invalid levelid or size" });
+      return;
+    }
+
+    const sizeInt = parseInt(size) || 10;
+    const result = await elasticClientInstance.search({
+      index: "logs",
+      body: {
+        query: {
+          term: {
+            level: levelid,
+          },
+        },
+      },
+      size: sizeInt,
+    });
+    res.send(result.hits.hits.map((hit) => hit._source));
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get level search" });
+  }
+};
+
+const getMessageSearch = async (req: Request, res: Response) => {
+  try {
+    const { messageinfo } = req.params;
+    if (!messageinfo) {
+      res.status(400).send({ message: "Invalid messageinfo" });
+      return;
+    }
+    const result = await elasticClientInstance.search({
+      index: "logs",
+      query: {
+        match: {
+          message: messageinfo,
+        },
+      },
+    });
+    console.log(result);
+    res.send(result.hits.hits.map((hit) => hit._source));
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get message search" });
+  }
+};
+
+const unifiedSearch = async (req: Request, res: Response) => {
+  try {
+    const { resourceId, traceId, timestamp, commit, spanId } = req.query;
+
+    if (!resourceId && !traceId && !commit && !spanId && !timestamp) {
+      res.status(400).send({ message: "Invalid parameters" });
+      return;
+    }
+
+    let termQuery;
+
+    switch (true) {
+      case !!resourceId:
+        termQuery = { resourceId };
+        break;
+      case !!traceId:
+        termQuery = { traceId };
+        break;
+      case !!commit:
+        termQuery = { commit };
+        break;
+      case !!spanId:
+        termQuery = { spanId };
+        break;
+      case !!timestamp:
+        termQuery = { timestamp };
+        break;
+      default:
+        res.status(400).send({ message: "Invalid parameters" });
+        return;
+    }
+
+    const query = {
+      index: "logs",
+      body: {
+        query: {
+          term: termQuery,
+        },
+      },
+    };
+
+    const result = await elasticClientInstance.search(query);
+
+    res.send(result.hits.hits.map((hit) => hit._source));
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get message search" });
+  }
+};
+
+const getParentResourceId = async (req: Request, res: Response) => {
+  try {
+    const { resourceId } = req.params;
+
+    if (!resourceId) {
+      res.status(400).send({ message: "Invalid parameters" });
+      return;
+    }
+    const result = await elasticClientInstance.search({
+      index: "logs",
+      body: {
+        query: {
+          term: {
+            "metadata.parentResourceId": resourceId,
+          },
+        },
+      },
+    });
+
+    res.send(result.hits.hits.map((hit) => hit._source));
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve parentResourceId" });
+  }
+};
+
+export {
+  getLevelSearch,
+  getLogs,
+  getParentResourceId,
+  getMessageSearch,
+  getfullSearch,
+  querySearch,
+  saveLogs,
+  unifiedSearch,
+};
--- log-ingestor/src/routes/index.ts
+++ log-ingestor/src/routes/index.ts
@@ -1,17 +1,59 @@
+import { Request, Response } from "express";
 import { Router } from "express";
 import {
+  getLevelSearch,
   getLogs,
   getfullSearch,
   saveLogs,
+  getMessageSearch,
+  getParentResourceId,
+  unifiedSearch,
 } from "../controller/logs.controller";
 const router = Router();
+
+function debug(req: Request, res: Response) {
+  switch (req.method) {
+    case "GET":
+      console.log(req.query);
+      res.send(req.query);
+      break;
+    case "POST":
+      res.send(req.body);
+      break;
+    default:
+      res.send();
+      break;
+  }
+}
 
 /* ---------------------------------- POST ---------------------------------- */
 router.post("/logs", saveLogs);
 
 /* ---------------------------------- GET ----------------------------------- */
 router.get("/logs", getLogs);
-router.get("/logs/:query", getfullSearch);
+
+// ? FULL TEXT SEARCH
+router.get("/logs/search/:query", getfullSearch);
+
+// ? LEVEL SEARCH AND SIZE DEFAULT IS 10
+router.get("/logs/level/:levelid/:size", getLevelSearch);
+
+// ? MESSAGE USE MATCH
+router.get("/logs/message/:messageinfo", getMessageSearch);
+
+// ? UNIFIED SEARCH FOR TRACEID, SPANID, TIMESTAMP, COMMIT
+router.get("/logs/unisearch", unifiedSearch);
+
+// ? METADATA
+router.get("/logs/unisearch/metadata/:resourceId", getParentResourceId);
 
 const apiRoutes = router;
 export default apiRoutes;
+
+// ? REFERENCE
+
+// *  http://localhost:3000/api/logs/parentresource/0oyrljc0ee7i
+// *  http://localhost:3000/api/logs/unisearch?timestamp=2023-11-18T06:47:59.947Z
+// *  http://localhost:3000/api/logs/unisearch?spanId=0oyrljc0ee7i
+// *  http://localhost:3000/api/logs/unisearch?traceId=j0ko9ncilxf
+// *  http://localhost:3000/api/logs/unisearch?commit=3045r1s9s4y
